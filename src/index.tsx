7c7baab80303ef926a21b3bec2a7f781161a578b
omidvarahram <omid.varahram@yahoo.com>
2021-05-21 16:33:22 +1000

    * Added store, actions and reducers * Added interfaces for customer details

--- src/index.tsx
+++ src/index.tsx
@@ -1,17 +1,43 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
+
+import './index.scss';
+
+const envSetup = (): void => {
+  // to deactivate console.log() in production
+  if (process.env.NODE_ENV !== 'development') {
+    console.log = (msg: any) => {
+      return console.debug(msg);
+    };
+    console.error = (msg: any) => {
+      return console.debug(msg);
+    };
+    console.table = (msg: any) => {
+      return console.debug(msg);
+    };
+    console.clear();
+  } else {
+    console.clear();
+  }
+};
+
+class App extends React.Component<any, any> {
+  componentDidMount(): void {
+    envSetup();
+  }
+
+  public render() {
+    return (
+      <div>
+        App
+      </div>
+    )
+  }
+}
 
 ReactDOM.render(
   <React.StrictMode>
-    <App />
+    <App/>
   </React.StrictMode>,
   document.getElementById('root')
 );
-
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
